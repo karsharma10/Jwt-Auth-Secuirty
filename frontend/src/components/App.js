1a6f0d8a3e478d43c39493ab138c7a08591dce2a
Kartik Sharma <kash5740@colorado.edu>
2024-03-13 20:32:13 -0600

    Added FrontEnd Component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,13 @@
 import "./App.css"
 import Header from "./Header";
+import AppContent from "./AppContent";
 export default function App(){
     return(
         <div>
             <Header pageTitle = "Fronend authenticated with JWT"></Header>
+            <div>
+                <AppContent/>
+            </div>
         </div>
     )
 }
---
+++ frontend/src/components/WelcomeContent.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export default class WelcomeContent extends React.Component{
+    render() {
+        return(
+          <div>
+              <h1>Welcome</h1>
+              <p>Login to see protected content</p>
+          </div>
+        );
+    };
+}

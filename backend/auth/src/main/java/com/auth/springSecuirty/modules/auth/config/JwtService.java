be8781f5010cbe0c2d53727fa110e1e0a3eff63a
Kartik Sharma <kash5740@colorado.edu>
2024-03-16 09:25:52 -0600

    Added all dependicies to jwt service

--- backend/auth/src/main/java/com/auth/springSecuirty/modules/auth/config/JwtService.java
+++ backend/auth/src/main/java/com/auth/springSecuirty/modules/auth/config/JwtService.java
@@ -48,6 +48,14 @@ public class JwtService { //this class will provide all the jwt service that wil
         return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
 
+    private boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token){
+        return extractClaim(token, Claims::getExpiration);
+    }
+    
 
     private Claims extractAllClaims(String token){
         return Jwts

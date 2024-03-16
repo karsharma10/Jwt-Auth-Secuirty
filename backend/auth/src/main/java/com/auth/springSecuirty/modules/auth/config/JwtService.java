e3723a98180155b27f908557552445b09efafac0
Kartik Sharma <kash5740@colorado.edu>
2024-03-15 20:21:50 -0600

    added extract all claims feature

--- backend/auth/src/main/java/com/auth/springSecuirty/modules/auth/config/JwtService.java
+++ backend/auth/src/main/java/com/auth/springSecuirty/modules/auth/config/JwtService.java
@@ -2,6 +2,8 @@ package com.auth.springSecuirty.modules.auth.config;
 
 import io.jsonwebtoken.Claims;
 
+import java.util.function.Function;
+
 public class JwtService { //this class will provide all the jwt service that wil be needed
 
     private final String SECRET_KEY = "978b4bd050e85b6d227918c9f2f5e9641f3e837377625b15b0c9e39bcfed27fa"; //change in prod to be outside
@@ -9,5 +11,12 @@ public class JwtService { //this class will provide all the jwt service that wil
     public String extractUsername(String token){
         return extractClaim(token, Claims::getSubject);
     }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){ //this will Extract All Claims Within JWT Token
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    
 
 }

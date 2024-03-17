18729312c593ea6ce683774060de04451e4c1286
Kartik Sharma <kash5740@colorado.edu>
2024-03-17 15:51:30 -0600

    Added auth header retreval

--- backend/auth/src/main/java/com/auth/springSecuirty/modules/auth/config/JwtAuthenticationFilter.java
+++ backend/auth/src/main/java/com/auth/springSecuirty/modules/auth/config/JwtAuthenticationFilter.java
@@ -1,4 +1,29 @@
 package com.auth.springSecuirty.modules.auth.config;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter implements OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(@NonNull  HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization"); //this will get the authorization from header
+
+        String jwt = null;
+        String username = null;
+
+        
+
+    }
 }

28dbfb557bbdcebf0d312f708a147bfa204dbc28
Kartik Sharma <kash5740@colorado.edu>
2024-03-18 16:57:50 -0600

    Added SecuirtyConfiguratinFile

---
+++ backend/auth/src/main/java/com/auth/springSecuirty/modules/auth/config/SecurityConfiguration.java
@@ -0,0 +1,12 @@
+package com.auth.springSecuirty.modules.auth.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@Component
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    
+}

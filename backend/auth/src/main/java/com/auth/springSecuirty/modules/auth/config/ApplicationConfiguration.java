7fcd90b4279deb6686a7be2cf67bb1742baf887b
Kartik Sharma <kash5740@colorado.edu>
2024-03-18 19:30:01 -0600

    Added UserDetailsService

--- backend/auth/src/main/java/com/auth/springSecuirty/modules/auth/config/ApplicationConfiguration.java
+++ backend/auth/src/main/java/com/auth/springSecuirty/modules/auth/config/ApplicationConfiguration.java
@@ -1,11 +1,21 @@
 package com.auth.springSecuirty.modules.auth.config;
 
+import com.auth.springSecuirty.modules.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfiguration {
 
-    private final
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService(){ //find user in the database by using a lambda function:
+        return username -> userRepository.findByEmail(username).orElseThrow(()-> new UsernameNotFoundException("User not found!"));
+    }
+
 }

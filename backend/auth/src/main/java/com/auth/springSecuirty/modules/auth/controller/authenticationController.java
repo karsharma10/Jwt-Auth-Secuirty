5e180af43ea68eef49a7546aabce0942c533892b
Kartik Sharma <kash5740@colorado.edu>
2024-03-27 13:36:43 -0600

    Added Cookie To Be Saved To Headers

--- backend/auth/src/main/java/com/auth/springSecuirty/modules/auth/controller/authenticationController.java
+++ backend/auth/src/main/java/com/auth/springSecuirty/modules/auth/controller/authenticationController.java
@@ -2,8 +2,11 @@ package com.auth.springSecuirty.modules.auth.controller;
 
 import com.auth.springSecuirty.modules.auth.service.AuthenticationService;
 import com.auth.springSecuirty.modules.user.dto.UserDto;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,14 +23,33 @@ public class authenticationController {
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody UserDto userDto){
         if(userDto == null){
-            return new ResponseEntity<>("User Already Exists In The Database",HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>("Please Give User Credentials",HttpStatus.NO_CONTENT);
         }
         if(authenticationService.doesUserExist(userDto)){ //if the user exists in the database, then don't register them
-            return new ResponseEntity<>(HttpStatus.METHOD_NOT_ALLOWED);
+            return new ResponseEntity<>("User Already Exists In The Database",HttpStatus.METHOD_NOT_ALLOWED);
         }
         else{
             String registerToken = authenticationService.register(userDto);
             return new ResponseEntity<>(registerToken, HttpStatus.OK);
         }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody UserDto userDto, HttpServletResponse response){
+        if(userDto == null){
+            return new ResponseEntity<>("User Already Exists In The Database",HttpStatus.NO_CONTENT);
+        }
+        String token = authenticationService.authentication(userDto);
+
+        ResponseCookie cookie = ResponseCookie.from("accessToken", token)
+                                .httpOnly(true)
+                                .secure(false)
+                                .path("/")
+                                .maxAge(1800)
+                                .build();
+
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }

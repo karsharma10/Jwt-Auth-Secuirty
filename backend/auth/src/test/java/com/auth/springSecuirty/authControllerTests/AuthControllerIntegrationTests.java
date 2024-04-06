e9162590dd409d60d68e4d479f252cff62909594
Kartik Sharma <kash5740@colorado.edu>
2024-04-06 15:24:24 -0600

    Added test that unauthorized user cannot access authorized api

--- backend/auth/src/test/java/com/auth/springSecuirty/authControllerTests/AuthControllerIntegrationTests.java
+++ backend/auth/src/test/java/com/auth/springSecuirty/authControllerTests/AuthControllerIntegrationTests.java
@@ -2,12 +2,18 @@ package com.auth.springSecuirty.authControllerTests;
 
 
 import com.auth.springSecuirty.modules.auth.controller.AuthenticationController;
+import com.auth.springSecuirty.modules.demo.DemoController;
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -15,11 +21,29 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @Transactional
 public class AuthControllerIntegrationTests {
 
-    private AuthenticationController authenticationController;
+    private final AuthenticationController authenticationController;
+
+    private final DemoController demoController;
+
+    private final MockMvc mockMvc;
+
 
     @Autowired
-    public AuthControllerIntegrationTests(AuthenticationController authenticationController){
+    public AuthControllerIntegrationTests(AuthenticationController authenticationController, DemoController demoController, MockMvc mockMvc){
         this.authenticationController = authenticationController;
+        this.demoController = demoController;
+        this.mockMvc = mockMvc;
     }
 
+    @Test
+    public void testThatUnAuthorizedUserShouldNotAccessAuthorizedAPI() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/v1/demo-controller/hello-secured")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+
+        )
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
 }
